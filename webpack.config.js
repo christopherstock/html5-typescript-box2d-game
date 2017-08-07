4b2ec53ef0b45766f03a7ce336aa8536af713f88
Christopher Stock <email@christopherstock.de>
2017-08-07 12:26:09 +0200

    Setup npm and webpack skeleton.

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+
+var path = require('path');
+
+module.exports = {
+    entry: [
+
+        './lib/index.js'
+
+    ],
+    output: {
+        filename: 'bundled.js',
+        path:     path.resolve(__dirname, 'dist')
+    }
+};
