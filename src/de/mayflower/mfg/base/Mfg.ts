6c64febae8c70493cbf78640bcfdd62f53b05aa1
Christopher Stock <email@christopherstock.de>
2017-08-27 17:37:25 +0200

    Updated TODO list.

--- src/de/mayflower/mfg/base/Mfg.ts
+++ src/de/mayflower/mfg/base/Mfg.ts
@@ -5,30 +5,26 @@
     *   The main class contains the application's points of entry and termination.
     *
     *   TODO ASAP   Create 'line' obstacles (parallelograms) in order to simplify rotation.
-    *   TODO INIT   Modify starting point for all objects so they rotate around left top anchor.
-    *
+    *   TODO ASAP   Modify starting point for all objects so they rotate around left top anchor.
+    *   TODO ASAP   Improve Pass-through walls behaviour for all characters etc. ..
     *   TODO ASAP   Checkout all parameters of the collision filters!
     *   TODO ASAP   Check sprite or image clipping and scaling to player size?
     *   TODO ASAP   Skew image (sensor) for waving grass effect?
-    *   TODO ASAP   Add sprites.
-    *   TODO ASAP   Add images.
+    *   TODO ASAP   Add sprites and sprite class.
+    *   TODO ASAP   Add images and image class ?? Improve image usage for all game objects ..
     *   TODO ASAP   Create custom renderer that extends Matter.Render?
     *   TODO ASAP   Try discreet graphic style.
-    *
     *   TODO HIGH   Create different enemy move patterns.
     *   TODO INIT   Parallax bg.
     *   TODO INIT   Solve same body friction on different surfaces with different friction ... ( "staticFriction" )
-    *   TODO LOW    Add doors / level portals.
     *   TODO LOW    Create levels and sublevels?
     *   TODO LOW    Maximum camera ascend distance if player is superjumped upwards.
-    *
-    *   TODO HIGH   Disable horizontal movements while jumping?
+    *   TODO LOW    Disable horizontal movements while jumping?
     *   TODO LOW    Improve direction change in air. (no direction change till landed?)
     *   TODO LOW    Improve air behaviour of player on colliding!!
-    *
+    *   TODO WEAK   Add doors / level portals.
     *   TODO WEAK   Add menu keys for main menu and level map ..
     *   TODO WEAK   Implement nice changing gravity effects.
-    *   TODO WEAK   Improve Pass-through walls behaviour for all characters etc. ..
     *   TODO WEAK   Refactor camera class.
     *   TODO WEAK   Try fullscreen game in browser?
     *
@@ -38,7 +34,7 @@
     export class Mfg
     {
         /** The singleton instance of the game engine. */
-        public      static          game                    :mfg.MfgGame                = null;
+        public      static          game                    :mfg.MfgGame                    = null;
 
         /*****************************************************************************
         *   This method is invoked when the application starts.
